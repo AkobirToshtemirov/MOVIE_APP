5cbfdfaf3b91421900923efe4c3a4a60ccb12506
Akobir5 <https://github.com/Akobir5/flashup-promo>
2022-03-27 16:17:25 +0500

    finish this practice

--- js/script.js
+++ js/script.js
@@ -2,7 +2,61 @@
 
 const API_key = 'api_key=66d51fdaf1c5dc58a0b0cde186d28671';
 const baseURL = 'https://api.themoviedb.org/3';
-const API_URl =  baseURL +'/discover/movie?sort_by=popularity.desc&' + API_key;
+const API_url =  baseURL +'/discover/movie?sort_by=popularity.desc&' + API_key;
+const img_url = 'https://image.tmdb.org/t/p/w500'
 
-console.log(API_URl);
+// HTML DOM main
+
+const mainTag = document.querySelector('#main .container')
+const formInput = document.querySelector('#form')
+const searchBtn = document.querySelector
+
+getMovie(API_url)
+
+function getMovie(url) {
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    showMovies(data.results)
+    console.log(data.results);
+  })
+}
+
+function showMovies(data) {
+
+  mainTag.innerHTML = ''
+
+  data.forEach(movie => {
+    const {title, poster_path, vote_average, overview} = movie
+
+    const movieElem = document.createElement('div');
+    movieElem.classList.add('movie');
+    movieElem.innerHTML = `
+    <img src="${img_url + poster_path}" alt="${title}">
+  
+    <div class="movieInfo">
+      <h3>${title}</h3>
+      <span class="${getColor(vote_average)}">${vote_average}</span>
+    </div>
+    <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+    </div>
+    `
+
+    mainTag.appendChild(movieElem)
+  });
+}
+
+function getColor(vote) {
+  if(vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
 
