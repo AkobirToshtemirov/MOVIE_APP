e8fa852724bbc50094f01b1d9af441792708cd74
Akobir5 <https://github.com/Akobir5/flashup-promo>
2022-03-28 22:30:01 +0500

    add new size

--- js/script.js
+++ js/script.js
@@ -44,10 +44,10 @@ function showMovies(data) {
       <h3>${title}</h3>
       <span class="${getColor(vote_average)}">${vote_average}</span>
     </div>
-    <div class="overview">
-      <h3>Overview</h3>
-      ${overview}
-    </div>
+
+    
+      <button class="watchNow">Watch Now <svg stroke="currentColor" fill="#fff" stroke-width="0" viewBox="0 0 16 16" height="20px" width="30px" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path></svg></button>
+
     `
     link.appendChild(movieElem)
     mainTag.appendChild(link)
